1f5ef1562fbb5e69a3d1d8a6cf776e1598672717
name <name@git.com>
2017-03-01 22:26:13 +0100

    Added creating posts form

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
+
+from django.utils import timezone
 from blog.models import Post
+
+from .forms import PostForm
+
 
 # Create your views here.
 def index(request):
-	posts = Post.objects.all()
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request,'blog/index.html',
 		{ 'posts':posts  }) 
 
-def post_details(request,id):
-	try:
-		post = Post.objects.get(id=id)
-	except Post.DoesNotExist:
-		raise Http404('This item does not exist')
+def post_detail(request,pk):
+
+	post = get_object_or_404(Post,pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post})
+	
+def post_new(request):
+	if request.method =="POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_edit(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method =="POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_edit.html', {'form':form})
--- wingdings/urls.py
+++ wingdings/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     # url(r'^$', 'wingdings.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$',views.index, name='index'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name="post_new"),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^admin/', include(admin.site.urls)),
 ]
